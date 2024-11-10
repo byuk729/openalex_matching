58ff4791b7cf43fd4514dd421c2f1ffbbdfb27d3
byuk729 <108688462+byuk729@users.noreply.github.com>
2024-11-10 11:37:10 -0500

    updated liscense, readme, and setup

---
+++ README.md
@@ -0,0 +1,8 @@
+#OpenAlex Matching
+
+##Description
+
+OpenAlex Matching is a Python package designed to match research authors with their corresponding OpenAlex ID using filters such as institutional publication history, author primary topics, and ORCID IDs. The package also includes functionality to read and output data in CSV format. 
+
+
+
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name= "openalex_matching",  
     version = "0.1",  
-    author="Your Name",  # Your name or organization
-    author_email="your.email@example.com",  # Your email address
-    description="A Python package for matching people using OpenAlex data",  # Short description of your package
+    author="Bryan Yuk", 
+    author_email= "dzg5cg@virginia.edu",  
+    description="A Python package for matching people with their OpenAlex ID using OpenAlex data",  
     long_description= open('README.md').read(),  # Long description (usually from README)
     long_description_content_type='text/markdown',  # Format of the long description (Markdown)
-    url="https://github.com/yourusername/openalex-matching",  # URL of your project (GitHub, etc.)
+    url="https://github.com/byuk729/openalex_matching",  # URL of your project (GitHub, etc.)
     packages=find_packages(),  # Automatically find all packages in your directory
     install_requires=[
         "requests",  # List of dependencies
